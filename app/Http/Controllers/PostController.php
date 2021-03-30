de902fa29fca076692d0f9dbc2238d5b8821441c
Vanilson02 <vanrock02@hotmail.com>
2021-03-30 12:10:22 -0300

    d volta

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     //
+    public function index()
+    {   
+        $posts = Post::get();
+        
+        return view('admin.posts.index',compact('posts'));
+    }
 }
